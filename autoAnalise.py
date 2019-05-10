337401f3c000c27f4f1688d7e031d385f8916cef
jonasLAraujo1 <42850141+jonasLAraujo1@users.noreply.github.com>
2019-05-10 11:46:58 -0300

    Update autoAnalise.py

--- autoAnalise.py
+++ autoAnalise.py
@@ -3,16 +3,25 @@ from os import system
 from datetime import datetime
 import os.path
 import sys
+import modulos
+
 #definição de cor para erro
 VERMELHO   = "\033[1;31m"
 NORMAL = "\033[0;0m"
-#
-import modulos
-parametros=modulos.config
+
+# carrega as configurações
+config=open("configuracao.conf",'r')
+parametros=config.readlines()
+
+#inicia o arquivo de logs
 arquivo=open("./log",'a')
+
 horario=datetime.now().strftime('%d/%m/%y   %H:%M')
-arquivo.write('Inicio: '+horario+'\n')
-genoma= sys.argv[1]
+arquivo.write('Inicio: '+horario+'\n') # grava a hora de inicio da Análise no log
+
+genoma= sys.argv[1] #pega o nome do arquivo pasado como segundo argumento
+
+# faz a verificação do tipo de arquivo que será análisado(fasta ou fna)
 if genoma[-3]=="f":
 	saida=genoma.replace('.fna','')
 elif genoma[-3]=="s":
@@ -20,46 +29,57 @@ elif genoma[-3]=="s":
 else:
 	print(VERMELHO+"ERRO!\nTipo de Arquivo Inválido\n"+NORMAL)
 	exit()
+#verifica se o arquivo tabular do genoma já existe
 arquivoTab=os.path.exists('./'+saida+".tab")
 opcao=0
 if (arquivoTab):
 	print('arquivo ' +genoma+' tabular já existe \nQuer pular a Análise do RepeatMasker? \n S ou N?')
 	opcao=input()
-
+# refaz a análise (por escolha do usuário)
 if (opcao == 'n' or opcao ==  'N'):
 	numero_proc= input("Digite o Numero de Processadores que Será Usado;\n")
+	""" tenta chamar o programa REpeatMasker 
+	se estiver tudo certo ele vai fazer os demais processos
+		*nota01 parametros[0] é a primeira linha do arquivo configuracao.conf 
+		 que contem o caminho do RepeatMasker
+		 *nota02 parametros[1] é a seginda linha do arquivo configuracao.conf 
+		 que contem o Email que vai ser enviado os alertas""""
+	
 	try:
-		system(parametros[0]" "+ numero_proc+" -s "+genoma)
-		system("awk -v OFS='\t' '$1=$1' "+genoma+".out > "+saida+".tab")
-		system("awk '{ print $10, $11 }' "+saida+".tab > "+saida+"colunasDuplas.tab")
-		modulos.enviar_email("Analise Finalizada")
+		system(parametros[0]" "+ numero_proc+" -s "+genoma) # *nota01
+		system("awk -v OFS='\t' '$1=$1' "+genoma+".out > "+saida+".tab") # transforma a saída em um arquivo tabular
+		system("awk '{ print $10, $11 }' "+saida+".tab > "+saida+"colunasDuplas.tab") # cria um arquivo coma as colunas 10 e 11 da saída
+		modulos.enviar_email("Analise Finalizada",parametros[1]) # *nota 02
 	except:
-		print(VERMELHO+"ERRO Na Analise do Arquivo: "+genoma+NORMAL)	
-		modulos.enviar_email("ERRO na Analise do Arquivo: "+genoma)
-		exit()
-	estrutura=modulos.indexar_contar(saida+"colunasDuplas.tab")
-	modulos.criar_txt(estrutura,saida)
+		print(VERMELHO+"ERRO Na Analise do Arquivo: "+genoma+NORMAL) # mensagem de erro na tela	
+		modulos.enviar_email("ERRO na Analise do Arquivo: "+genoma,parametros[1]) # mensagem de erro no email
+		exit() # é pra fechar o programa
+	estrutura=modulos.indexar_contar(saida+"colunasDuplas.tab") # faz a contagem e pré-estrutura os dados
+	modulos.criar_txt2(estrutura,saida) # cria o arquivo de saida com os dados
 
 elif(opcao == 's' or opcao ==  'S'):
+	""" Faz os processos de estruturação dos dados caso já exista um arquivo tabular(Por escolha do Usuário) """"
 	system("awk '{ print $10, $11 }' "+saida+".tab > "+saida+"colunasDuplas.tab")
 	estrutura=modulos.indexar_contar(saida+"colunasDuplas.tab")
-	modulos.criar_txt(estrutura,saida)
+	modulos.criar_txt2(estrutura,saida ) 
 
 else:
+	"""execução padrão dos processos (quando não existe um arquivo tabular) """
 	numero_proc= input("Digite o Numero de Processadores que Será Usado;\n")
 	try:
 		system(parametros[0]" "+ numero_proc+" -s "+genoma)
 		system("awk -v OFS='\t' '$1=$1' "+genoma+".out > "+saida+".tab")
 		system("awk '{ print $10, $11 }' "+saida+".tab > "+saida+"colunasDuplas.tab")
-		modulos.enviar_email("Analise Finalizada")
+		modulos.enviar_email("Analise Finalizada",parametros[1])
 	except:
 		print(VERMELHO+"ERRO Na Analise do Arquivo: "+genoma+NORMAL)	
-		modulos.enviar_email("ERRO na Analise do Arquivo: "+genoma)
+		modulos.enviar_email("ERRO na Analise do Arquivo: "+genoma,parametros[1])
 		exit()
 	
 	estrutura=modulos.indexar_contar(saida+"colunasDuplas.tab")
-	modulos.criar_txt(estrutura,saida)
+	modulos.criar_txt2(estrutura,saida)
 
-arquivo.write('Fim: '+horario+'\n')
-horario=datetime.now().strftime('%d/%m/%y   %H:%M')
-arquivo.close()
+
+horario=datetime.now().strftime('%d/%m/%y   %H:%M') # Pega a hora do fim dos processos
+arquivo.write('Fim: '+horario+'\n') # escreve no arquivo de log quando terminou os processos
+arquivo.close() # fecha o arquivo de log
